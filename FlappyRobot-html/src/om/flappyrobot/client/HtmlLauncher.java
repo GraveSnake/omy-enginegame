ce5278c8f2e1276a78ea366092e79ffde705acd0
yasser.ouaftouh@gmail.com <yasser.ouaftouh@gmail.com@2b0e6908-d3cb-11f6-2aa4-29747d533aab>
2014-04-29 04:21:07 +0000

    Commit FlappyRobot HTML Project

---
+++ FlappyRobot-html/src/om/flappyrobot/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package om.flappyrobot.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import om.flappyrobot.FlappyRobotGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+	@Override
+	public GwtApplicationConfiguration getConfig() {
+		return new GwtApplicationConfiguration(480, 320);
+	}
+
+	@Override
+	public ApplicationListener getApplicationListener() {
+		return new FlappyRobotGame();
+	}
+}
